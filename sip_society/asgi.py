c0d15680c9ab27107d55e033daee4180bdfb4412
funkybeppe <lucabarbero1996@gmail.com>
2023-04-20 10:47:11 +0000

    Project setup

---
+++ sip_society/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sip_society project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sip_society.settings')
+
+application = get_asgi_application()
