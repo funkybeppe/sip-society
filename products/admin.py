f9c56629436dbd2d795fe20de21a9e780ad60e2b
funkybeppe <lucabarbero1996@gmail.com>
2023-04-25 12:24:38 +0000

    add product and category model, registered fields in admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,30 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    """Class for displaying products in admin panel"""
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'country',
+        'year',
+        'price',
+        'rating',
+    )
+    readonly_fields = ('rating', )
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """Class for displaying categories in admin panel"""
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
 admin.site.register(Product)
 admin.site.register(Category)
