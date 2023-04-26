91f51bc309dbba4ba5b285501335ef0d5fb66394
funkybeppe <lucabarbero1996@gmail.com>
2023-04-26 11:07:36 +0000

    add products to database

--- products/admin.py
+++ products/admin.py
@@ -27,5 +27,5 @@ class CategoryAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(Product)
-admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
