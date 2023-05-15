211f75cea27ef3cb5b283a24107cb608c446226b
funkybeppe <lucabarbero1996@gmail.com>
2023-05-15 12:05:01 +0000

    add remove from bag view to delete product from shopping bag

--- bag/views.py
+++ bag/views.py
@@ -14,7 +14,6 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    size = None
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
@@ -44,9 +43,18 @@ def adjust_bag(request, item_id):
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
-    bag = request.session.get('bag', {})
+    try:
+        bag = request.session.get('bag', {})
 
-    bag.pop(item_id)
+        if item_id in list(bag.keys()):
+            del bag[item_id]
+            if not bag[item_id]:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
 
-    request.session['bag'] = bag
-    return HttpResponse(status=200)
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
