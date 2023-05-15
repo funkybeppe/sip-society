a7861e2bc8b74c60d8fe1e41dde8ce66b5703cf5
funkybeppe <lucabarbero1996@gmail.com>
2023-05-15 12:33:24 +0000

    remove get object or 404 module as it is not needed

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
