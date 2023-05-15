94f7c9336c38186a9c661a04adb5e712cb4b11d8
funkybeppe <lucabarbero1996@gmail.com>
2023-05-15 09:51:19 +0000

    fixed urls for product detail page

--- products/urls.py
+++ products/urls.py
@@ -2,12 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', views.all_products, name='products'),
     path('<product_id>', views.product_detail, name='product_detail'),
-=======
-    path('', views.Products.as_view(), name='products'),
-    path('product_detail/<product_id>/', views.ProductDetail.as_view(),
-         name='product_detail'),
->>>>>>> main
 ]
