ab28b9cc0f38b99e5edbb46856b0fd9fd81a9a43
funkybeppe <lucabarbero1996@gmail.com>
2023-05-15 11:29:29 +0000

    add function to remove product from bag

--- bag/views.py
+++ bag/views.py
@@ -39,3 +39,14 @@ def adjust_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    bag = request.session.get('bag', {})
+
+    bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
