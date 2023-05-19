b5c817c41099181884a7e23fca12cc29674cef8c
funkybeppe <lucabarbero1996@gmail.com>
2023-05-19 19:46:16 +0000

    add code to trigger spinner overlay and fade after one second

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -53,6 +53,8 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
