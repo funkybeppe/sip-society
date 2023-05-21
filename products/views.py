999f1b1c2b3fe01d9fbb5ba566eb7e26bfec6a30
funkybeppe <lucabarbero1996@gmail.com>
2023-05-21 15:09:10 +0000

    modify search criteria as it was not working

--- products/views.py
+++ products/views.py
@@ -43,7 +43,9 @@ def all_products(request):
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = Q(name__icontains=query) | Q(region__icontains=query) | \
+                Q(country__icontains=query) | Q(grapes__icontains=query) | \
+                Q(style__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
