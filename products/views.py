04ecc661204f2f06fe2cd73d5d4c566ce2d56f68
funkybeppe <lucabarbero1996@gmail.com>
2023-05-21 15:13:23 +0000

    add view to add product

--- products/views.py
+++ products/views.py
@@ -70,3 +70,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
