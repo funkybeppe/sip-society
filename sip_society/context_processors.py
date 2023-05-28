f817c7a12968230afc3f8307e39bdaaf54762f88
funkybeppe <lucabarbero1996@gmail.com>
2023-05-28 16:17:08 +0000

    add robots and sitemap files, add subscribe form

--- sip_society/context_processors.py
+++ sip_society/context_processors.py
@@ -1,4 +1,5 @@
 from django.core.serializers import serialize
+from newsletter.forms import AddSubscriber
 from products.models import Product
 
 
@@ -6,4 +7,11 @@ def add_products_list_to_context(request):
     """Method to return a list of products as context"""
     return {
         'products_json': serialize('json', Product.objects.all())
+    }
+
+
+def add_subscription_form_to_context(request):
+    """Method to return subscription form as context"""
+    return {
+        'add_subscriber_form': AddSubscriber
     }
--- sip_society/settings.py
+++ sip_society/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+from os import path
 import dj_database_url
+
+
+if path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
